fd9933056c8d7f9a08072aa325e56f10ceda04cd
Boojux <iamboojux@gmail.com>
2025-07-01 10:32:31 +0800

    add solution for counting possible original typed strings

---
+++ playground/src/main/java/org/example/FindTheOriginalTypedStringI/Solution.java
@@ -0,0 +1,13 @@
+package org.example.FindTheOriginalTypedStringI;
+
+class Solution {
+    public int possibleStringCount(String word) {
+        int ans = 0;
+        for(int i = 1; i < word.length(); ++i){
+            if(word.charAt(i) == word.charAt(i-1)){
+                ++ans;
+            }
+        }
+        return ans+1;
+    }
+}
