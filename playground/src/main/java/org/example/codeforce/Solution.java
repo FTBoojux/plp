3a774500d9d013f26b3e14850227bed3ff088acf
Boojux <iamboojux@gmail.com>
2025-07-02 11:41:18 +0800

    add initial solution structure with main method and input handling

---
+++ playground/src/main/java/org/example/codeforce/Solution.java
@@ -0,0 +1,18 @@
+package org.example.codeforce;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in =  new Scanner(System.in);
+        int t = in.nextInt();
+        Solution solution = new Solution();
+        while (t-- > 0) {
+            solution.solve(in);
+        }
+    }
+
+    public void solve(Scanner in) {
+
+    }
+}
