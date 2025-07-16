4a2a90467fb64e1225108cb312f788fcde9afa22
Boojux <iamboojux@gmail.com>
2025-07-16 11:15:27 +0800

    add maximumLength method to Solution for finding the maximum length of valid subsequence

---
+++ playground/src/main/java/org/example/leetcode/FindtheMaximumLengthofValidSubsequenceI/Solution.java
@@ -0,0 +1,18 @@
+package org.example.leetcode.FindtheMaximumLengthofValidSubsequenceI;
+
+class Solution {
+    public int maximumLength(int[] nums) {
+        int ans = 0;
+        int[][] patterns = {{0,0},{0,1},{1,0},{1,1}};
+        for (int[] pattern : patterns) {
+            int cnt = 0;
+            for (int num : nums) {
+                if(num % 2 == pattern[cnt % 2]){
+                    ++cnt;
+                }
+            }
+            ans = Math.max(ans, cnt);
+        }
+        return ans;
+    }
+}
