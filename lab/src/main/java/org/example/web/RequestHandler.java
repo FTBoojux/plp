787d92ddb06dc152289edcf7f26d24114f05c5ec
Boojux <iamboojux@gmail.com>
2025-07-17 13:57:16 +0800

    remove default implementation of get method in RequestHandler interface

--- lab/src/main/java/org/example/web/RequestHandler.java
+++ lab/src/main/java/org/example/web/RequestHandler.java
@@ -3,9 +3,6 @@ package org.example.web;
 import org.example.web.request.HttpRequest;
 
 public interface RequestHandler<T> {
-    default Object get(HttpRequest<T> httpRequest){
-        String msg = String.join("", "The implement: ", getUrl(), "have not be implemented yet!");
-        throw new RuntimeException(msg);
-    }
+    Object get(HttpRequest<T> httpRequest);
     String getUrl();
 }
