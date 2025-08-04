743ff4be3f3c3d65577a688df4d5a25b2dcc7b34
Boojux <iamboojux@gmail.com>
2025-08-04 09:31:40 +0800

    add solution to calculate maximum number of fruits in two types of baskets

---
+++ playground/src/main/java/org/example/leetcode/totalFruit/Solution.java
@@ -0,0 +1,22 @@
+package org.example.leetcode.totalFruit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        for(int left = 0, right = 0; right < fruits.length; ++right){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size() > 2){
+                map.put(fruits[left],map.get(fruits[left])-1);
+                if(map.get(fruits[left]) == 0){
+                    map.remove(fruits[left]);
+                }
+            }
+            ans = Math.max(ans, right-left+1);
+        }
+        return ans;
+    }
+}
