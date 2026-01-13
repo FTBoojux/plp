1ac2e90025362f28a52e6131498844f428f5bb88
Boojux <iamboojux@gmail.com>
2026-01-13 10:34:09 +0800

    add solution for problem c1072 A

---
+++ playground/src/main/java/org/example/codeforce/c1072/A/Solution.java
@@ -0,0 +1,23 @@
+package org.example.codeforce.c1072.A;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in =  new Scanner(System.in);
+        int t = in.nextInt();
+        Solution solution = new Solution();
+        while (t-- > 0) {
+            solution.solve(in);
+        }
+    }
+
+    public void solve(Scanner in) {
+        int n = in.nextInt();
+        if ( n <= 3) {
+            System.out.println(n);
+        } else {
+            System.out.println(n%2);
+        }
+    }
+}
